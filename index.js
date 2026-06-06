2bc553208c6b7063e63daf33833594e3598d6c3d
Pallab Karmakar <ookarmakarpallab1212@gmail.com>
2026-06-07 00:49:31 +0530

    fix: add new mesage on home route

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.json({ message: 'Hello, World!', status: 200 })
+    res.json({ message: 'Depali, Rachit, Kunal we have many pro devs', status: 200 })
 });
 
 app.listen(PORT, () => {
