771c3b902d4784f1bafc91e4ae70b38ca6782b57
Pallab Karmakar <ookarmakarpallab1212@gmail.com>
2026-06-07 01:25:57 +0530

    feat: add a home page pnd

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@
 FROM node:22-alpine
 
 COPY index.js /project/pallab/index.js
+COPY public /project/pallab/public
 COPY package.json /project/pallab/package.json
 COPY package-lock.json /project/pallab/package-lock.json
 
--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.get('/', (req, res) => {
-    res.json({ message: 'Asli Pro AP2S 2026', status: 200 })
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
